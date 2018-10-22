b6348a858e8d4312ca3124309f48930343059b31
scripter <scripter.proger@gmail.com>
2018-10-22 22:10:56 +0300

    lesson "bits"

--- Common/Common/Lesson_bits.cpp
+++ Common/Common/Lesson_bits.cpp
@@ -1,6 +1,34 @@
 #include "stdafx.h"
 #include "lesson_bits.h"
 
+void bitFieldTest()
+{
+	struct bitField1
+	{
+		char a : 3;
+		char b : 3;
+		char c : 2;
+	};
+	std::cout << "Test \"bitField\": " << sizeof(bitField1) << "\n"; // 1 bite
+
+	struct bitField2
+	{
+		char a : 3;
+		char b : 3;
+		char c : 3;
+	};
+	std::cout << "Test \"bitField\": " << sizeof(bitField2) << "\n"; // 2 bite
+
+	struct bitField3
+	{
+		int a : 3;
+		char b : 3;
+		char c : 3;
+	};
+	std::cout << "Test \"bitField\": " << sizeof(bitField3) << "\n"; // 8 bite, multiple of max type size
+}
+
 void Lesson_bits::run()
 {
+	bitFieldTest();
 }
