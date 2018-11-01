64a2e412950c13d1443c7f2ded3deb00272813c6
scripter <scripter.proger@gmail.com>
2018-11-01 13:31:38 +0300

    lesson "stl"

--- Common/Common/lesson_stl.cpp
+++ Common/Common/lesson_stl.cpp
@@ -12,13 +12,13 @@ void sortTest()
 		int num;
 	};
 
-	struct WorkerSorter
+	struct DataSorter
 	{
 		bool operator()(const Data& a, const Data& b) const { return a.num < b.num; }
 	};
 
 	std::vector<Data> vec = { { "d", 4 }, { "c", 3 }, {"a", 1}, { "b", 2 }, };
-	std::sort(vec.begin(), vec.end(), WorkerSorter());
+	std::sort(vec.begin(), vec.end(), DataSorter());
 
 	std::cout << "Test \"sort\": ";
 	for (auto& data : vec) // [range-based for loop]
@@ -28,7 +28,39 @@ void sortTest()
 	std::cout << "\n";
 }
 
+/****************************************************/
+/*                     insert                       */
+/****************************************************/
+struct IntSorter
+{
+	bool operator()(int a, int b) const { return a < b; }
+};
+
+void insertInt(std::vector<int>& vec, const int i)
+{
+	auto it = std::lower_bound(vec.begin(), vec.end(), i, IntSorter());
+	vec.insert(it, i);
+}
+
+void insertTest()
+{
+	std::vector<int> vec;
+
+	insertInt(vec, 4);
+	insertInt(vec, 1);
+	insertInt(vec, 2);
+	insertInt(vec, 3);
+
+	std::cout << "Test \"insert\": ";
+	for (auto& data : vec) // [range-based for loop]
+	{
+		std::cout << data << ", ";
+	}
+	std::cout << "\n";
+}
+
 void Lesson_stl::run()
 {
 	sortTest();
+	insertTest();
 }
