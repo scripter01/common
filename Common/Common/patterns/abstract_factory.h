eeb0482707aa4239bd51386795abfcf1b62e8311
Scripter <scripter.proger@gmail.com>
2018-12-02 23:19:12 +0300

    abstract factory

--- Common/Common/patterns/abstract_factory.h
+++ Common/Common/patterns/abstract_factory.h
@@ -1,5 +1,55 @@
 #ifndef ABSTRACT_FACTORY_H
 #define ABSTRACT_FACTORY_H
 
+class SoldierAnt
+{
+public:
+	virtual void info() = 0;
+	virtual ~SoldierAnt() {}
+};
+
+class WorkerAnt
+{
+public:
+	virtual void info() = 0;
+	virtual ~WorkerAnt() {}
+};
+
+class AntsFactory
+{
+public:
+	virtual SoldierAnt* createSoldierAnt() = 0;
+	virtual WorkerAnt* createWorkerAnt() = 0;
+	virtual ~AntsFactory() {}
+};
+
+class RedSoldierAnt: public SoldierAnt
+{
+public:
+	void info() { printf("Red Soldier Ant"); }
+};
+
+class RedWorkerAnt: public WorkerAnt
+{
+public:
+	void info() { printf("Red Worker Ant"); }
+};
+
+class RedAntsFactory : public AntsFactory
+{
+public:
+	SoldierAnt* createSoldierAnt() { return new RedSoldierAnt; }
+	WorkerAnt* createWorkerAnt() { return new RedWorkerAnt; }
+};
+
+class AbstractFactoryTest
+{
+public:
+	void createAnts(AntsFactory& factory) 
+	{
+		// use factory
+	}
+};
+
 
 #endif //ABSTRACT_FACTORY_H
