e26d7685f39eeb5e1a5f8f2e1dc5615c93c1384d
Scripter <scripter.proger@gmail.com>
2020-09-29 21:45:06 +0300

    update pattern: command

--- patterns/command.cpp
+++ patterns/command.cpp
@@ -37,7 +37,7 @@ public:
 			mButtonY->execute();
 	}
 
-private:
+protected::
 	bool isPressedX() const { return false; }
 	bool isPressedY() const { return false; }
 
@@ -45,3 +45,42 @@ private:
 	Command* mButtonX;
 	Command* mButtonY;
 };
+
+//////////////////////////////////////////////////////////////////////////
+
+class MoveUnitCommand : public Command
+{
+public:
+	MoveUnitCommand(Unit* unit, int x, int y) :
+		mUnit(unit),
+		mX(x),
+		mY(y)
+	{}
+
+	virtual void execute()
+	{
+		mOldX = mUnit->getX();
+		mOldY = mUnit->getY();
+		mUnit->moveTo(mX, mY);
+	}
+
+	virtual void undo()
+	{
+		mUnit->moveTo(mOldX, mOldY);
+	}
+
+private:
+	Unit* mUnit = nullptr;
+	int mOldX, mOldY = 0;
+	int mX, mY = 0;
+};
+
+/*
+older [CMD][CMD][CMD][CMD][CMD][CMD][CMD][CMD] newer
+                       |
+              (undo)current(redo)
+
+ огда пользователь выбирает "отмена", мы отмен€ем текущую команду и сдвигаем указатель на одну позицию назад.
+ огда мы выполн€ем повтор, мы перемещаем указатель на позицию вперед и выполн€ем команду. 
+ огда пользователь после отмены команды выполн€ем=т новую команду, все содержимое списка после текущей команды выбрасываетс€
+*/
